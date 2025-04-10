9c97942dcb5655c638c2d159553e67ad4aea17f8
pirlo <piale820@gmail.com>
2025-04-10 02:40:41 -0500

    P1

--- conexion.php
+++ conexion.php
@@ -1,7 +1,7 @@
 <?php
 include 'configuration.php';
 
-$conexion = new mysqli('localhost', 'root', '', 'phplogin');
+$conexion = new mysqli($server, $user, $pass, $db);
 if (mysqli_connect_errno()) {
     echo "No conectado",mysqli_connect_error();
     exit();
